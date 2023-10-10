070eeee97cf6105dc1ed31aba7d2726bd5b5130b
Ginasrrhmwti <141693381+Ginasrrhmwti@users.noreply.github.com>
2023-10-10 15:38:37 +0700

    Add files via upload

---
+++ biodata.js
@@ -0,0 +1,8 @@
+let nama = "Gina sri rahmawati";
+document.getElementById("nama").innerHTML = nama
+let alamat = "Kp.cidahu";
+document.getElementById("alamat").innerHTML = alamat
+let ttg = "Tasikmalaya,28 desember 2006";
+document.getElementById("ttg").innerHTML = ttg
+let hoby = "turu";
+document.getElementById("hoby").innerHTML = hoby
